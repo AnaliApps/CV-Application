4325ddc2ab6156729b37efb26b16beb2186abeca
AnaliApps <ronoanali5@gmail.com>
2024-10-25 21:27:41 +0300

    Added styling for AddCVForm component and EditCVForm component - Styled AddCVForm and EditCVForm components to make them look presentable. - Styling for these two components is now complete.

--- cv-application-app/src/components/EditCVForm.jsx
+++ cv-application-app/src/components/EditCVForm.jsx
@@ -6,47 +6,75 @@ function EditCVForm({fullName,setFullName,email,setEmail,phone,setPhone,schoolNa
             <div className="genInformation">
                 <fieldset>
                     <legend>General Personal Information</legend>
-                    <label htmlFor="fname">Name:</label>
-                    <input type="text" name="fname" id="fname" placeholder="Enter your fullname.." 
-                    value={fullName}
-                    onChange={(e) => {setFullName(e.target.value)}} />
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" id="email" placeholder="johndoe@gmail.com" value={email} onChange={(e)=>setEmail(e.target.value)} />
-                    <label htmlFor="phone">Phone:</label>
-                    <input type="tel" name="phone" id="phone" placeholder="0700200100" value={phone} onChange={(e)=>setPhone(e.target.value)}/>
+                    <div>
+                        <div>
+                            <label htmlFor="fname">Name:</label>
+                            <input type="text" name="fname" id="fname" placeholder="Enter your fullname.." 
+                            value={fullName}
+                            onChange={(e) => {setFullName(e.target.value)}} />
+                        </div>
+                        <div>
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" name="email" id="email" placeholder="johndoe@gmail.com" value={email} onChange={(e)=>setEmail(e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor="phone">Phone:</label>
+                            <input type="tel" name="phone" id="phone" placeholder="0700200100" value={phone} onChange={(e)=>setPhone(e.target.value)}/>
+                        </div>
+                    </div>
                 </fieldset>
                 
             </div>
             <div className="Education">
                 <fieldset>
                     <legend>Educational Background</legend>
-                    <label htmlFor="schoolname">School Name:</label>
-                    <input type="text" name="schoolname" id="schoolname" placeholder="Enter your School name.." 
-                    value={schoolName}
-                    onChange={(e)=>setSchoolName(e.target.value)} />
-                    <label htmlFor="studyTitle">Title of Study:</label>
-                    <input type="text" name="studyTitle" id="studyTitle" placeholder="Enter course studied" value={studyTitle} onChange={(e)=>setStudyTitle(e.target.value)} />
-                    <label htmlFor="dateStudy">Date of Study:</label>
-                    <input type="date" name="dateStudy" id="dateStudy" placeholder="0700200100" value={dateStudy} onChange={(e)=>setdateStudy(e.target.value)}/>
+                    <div>
+                        <div>
+                            <label htmlFor="schoolname">School Name:</label>
+                            <input type="text" name="schoolname" id="schoolname" placeholder="Enter your School name.." 
+                            value={schoolName}
+                            onChange={(e)=>setSchoolName(e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor="studyTitle">Title of Study:</label>
+                            <input type="text" name="studyTitle" id="studyTitle" placeholder="Enter course studied" value={studyTitle} onChange={(e)=>setStudyTitle(e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor="dateStudy">Date of Study:</label>
+                            <input type="date" name="dateStudy" id="dateStudy" placeholder="0700200100" value={dateStudy} onChange={(e)=>setdateStudy(e.target.value)}/>
+                        </div>
+                    </div>
                 </fieldset>
             </div>
             <div className="workexperience">
                 <fieldset>
                     <legend>Work Experience</legend>
-                    <label htmlFor="companyname">Company Name:</label>
-                    <input type="text" name="companyname" id="companyname" placeholder="Enter your Company name.." 
-                    value={companyName}
-                    onChange={(e)=>setCompanyName(e.target.value)} />
-                    <label htmlFor="positionTitle">Position Title:</label>
-                    <input type="text" name="positionTitle" id="positionTitle" placeholder="Enter the position you held.." 
-                    value={positionTitle}
-                    onChange={(e)=>setPositionTitle(e.target.value)} />
-                    <label htmlFor="responsibilityDesc">Reponsibility Description:</label>
-                    <textarea name="responsibilityDesc" id="responsibilityDesc" value={responsibilityDesc} cols={20} rows={10} onChange={(e)=>setResponsibilityDesc(e.target.value)}></textarea>
-                    <label htmlFor="dateWorkedFrom">Date From:</label>
-                    <input type="date" name="dateWorkedFrom" id="dateWorkedFrom"  value={dateWorkedFrom} onChange={(e)=>setDateWorkedFrom(e.target.value)}/> 
-                    <label htmlFor="dateWorkedTo">Date To:</label>
-                    <input type="date" name="dateWorkedTo" id="dateWorkedTo"  value={dateWorkedTo} onChange={(e)=>setDateWorkedTo(e.target.value)}/>      
+                    <div>
+                        <div>
+                            <label htmlFor="companyname">Company Name:</label>
+                            <input type="text" name="companyname" id="companyname" placeholder="Enter your Company name.." 
+                            value={companyName}
+                            onChange={(e)=>setCompanyName(e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor="positionTitle">Position Title:</label>
+                            <input type="text" name="positionTitle" id="positionTitle" placeholder="Enter the position you held.." 
+                            value={positionTitle}
+                            onChange={(e)=>setPositionTitle(e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor="responsibilityDesc">Description:</label>
+                            <textarea name="responsibilityDesc" id="responsibilityDesc" value={responsibilityDesc} cols={20} rows={10} onChange={(e)=>setResponsibilityDesc(e.target.value)}></textarea>
+                        </div>
+                        <div>
+                            <label htmlFor="dateWorkedFrom">Date From:</label>
+                            <input type="date" name="dateWorkedFrom" id="dateWorkedFrom"  value={dateWorkedFrom} onChange={(e)=>setDateWorkedFrom(e.target.value)}/>
+                        </div>
+                        <div>
+                            <label htmlFor="dateWorkedTo">Date To:</label>
+                            <input type="date" name="dateWorkedTo" id="dateWorkedTo"  value={dateWorkedTo} onChange={(e)=>setDateWorkedTo(e.target.value)}/> 
+                        </div>
+                    </div>      
                 </fieldset>
             </div>
             <button type="submit">Save</button>
